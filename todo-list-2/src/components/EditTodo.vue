641a6c0da51d121753691f72b9042da76f8633da
brianaruff <briananthonyruff@gmail.com>
2021-08-01 23:24:41 -0400

    update edit functionality with proper edit on the instance instead of all edits!

---
+++ todo-list-2/src/components/EditTodo.vue
@@ -0,0 +1,35 @@
+<template>
+   <div>
+      <input v-if="this.ediTable" v-model="editedText" type="text" />
+      <button @click="toggleEditable">Edit</button>
+      <button @click="editTodo">Finish Editing</button>
+   </div>
+</template>
+
+<script>
+export default {
+   name: 'EditTodo',
+   props: {
+      todoText: String,
+      index: Number,
+   },
+   data() {
+      return {
+         editedText: '',
+         ediTable: false,
+      };
+   },
+   methods: {
+      editTodo() {
+         this.ediTable = false;
+         this.$emit('editTodo', this.editedText, this.index);
+      },
+      toggleEditable() {
+         this.ediTable = !this.ediTable;
+      },
+   },
+};
+</script>
+
+<style>
+</style>
---
+++ todo-list-2/src/components/TodoList.vue
@@ -0,0 +1,75 @@
+<template>
+   <div>
+      <h1>Todo List</h1>
+      <input type="text" class="input" v-model="newTodo" />
+      <button @click="add">Add</button>
+      <ul
+         style="list-style-type: none"
+         v-for="(todo, index) in existingTodos"
+         :key="index"
+      >
+         <li>{{ index + 1 }}. {{ todo.text }}</li>
+         <button @click="deleteTodo(index)">Delete</button>
+         <EditTodo
+            @editTodo="handleEditTodo"
+            :todoText="todo.text"
+            :index="index"
+         />
+      </ul>
+   </div>
+</template>
+
+<script>
+import EditTodo from './EditTodo.vue';
+export default {
+   name: 'TodoList',
+   components: {
+      EditTodo,
+   },
+   data() {
+      return {
+         newTodo: '',
+         existingTodos: [
+            {
+               text: 'Learn Vue.js',
+               id: 1,
+            },
+            {
+               text: 'Learn JavaScript',
+               id: 2,
+            },
+            {
+               text: 'Build something awesome',
+               id: 3,
+            },
+            {
+               text: 'Make a cool game',
+               id: 4,
+            },
+            {
+               text: 'Have a good day',
+               id: 5,
+            },
+         ],
+      };
+   },
+   methods: {
+      add() {
+         this.existingTodos.push({
+            text: this.newTodo,
+            id: this.existingTodos.length + 1,
+         });
+      },
+      deleteTodo(index) {
+         this.existingTodos.splice(index, 1);
+      },
+      handleEditTodo(text, index) {
+         this.existingTodos[index].text = text;
+         this.editable = false;
+      },
+   },
+};
+</script>
+
+<style>
+</style>
